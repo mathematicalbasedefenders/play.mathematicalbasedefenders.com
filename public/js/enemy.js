fc2aa9dddf50c5e7d8efd2474f795d963bc02ad6
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2022-10-15 17:21:25 +0700

    change: "refactored" code in `enemy.js`

--- public/js/enemy.js
+++ public/js/enemy.js
@@ -18,71 +18,9 @@ class Enemy {
 
     this.stackLevel = 0;
 
-    if (!this.minified) {
-      this.updateSprite();
-    }
-  }
+    this.enemySprite;
 
-  checkIfOverlapping(other) {
-    let thisLeft = this.xPosition;
-    let thisRight = this.xPosition + this.width;
-
-    let thisTop = this.yPosition;
-    let thisBottom = this.yPosition + this.height;
-
-    let otherLeft = other.xPosition;
-    let otherRight = other.xPosition + other.width;
-
-    let otherTop = other.yPosition;
-    let otherBottom = other.yPosition + other.height;
-
-    if (
-      thisLeft < otherRight &&
-      thisRight > otherLeft &&
-      thisTop < otherBottom &&
-      thisBottom > otherTop
-    ) {
-      return true;
-    }
-    return false;
-  }
-
-  countOverlapping() {
-    let total = 0;
-    for (let enemy of game.enemyRenderStatus) {
-      if (enemy.enemyNumber === this.enemyNumber) {
-        // don't compare with itself
-        continue;
-      }
-      if (this.checkIfOverlapping(this, enemy)) {
-        total++;
-      }
-    }
-    return total;
-  }
-
-  updateSprite() {
     let enemyColor = createEnemyColor();
-
-    let red = (enemyColor & 0xff0000) >> 16;
-    let green = (enemyColor & 0x00ff00) >> 8;
-    let blue = enemyColor & 0x0000ff;
-    let minimum = Math.min(Math.min(red, green), blue) / 255;
-    let maximum = Math.max(Math.max(red, green), blue) / 255;
-
-    // create text sprite for value
-    let requestedValueTextStyleToUse = new PIXI.TextStyle({
-      fontFamily:
-        '"Computer Modern Math Italic", Computer Modern Unicode Serif',
-      fill:
-        settings.video.enemyColor == "blind"
-          ? "#eeeeee"
-          : (maximum + minimum) / 2 >= 0.5
-          ? "#000000"
-          : "#ffffff",
-      fontSize: 32
-    });
-
     if (document.querySelector("#custom-enemy-picture").querySelector("img")) {
       if (
         document.querySelector("#custom-enemy-picture").querySelector("img")
@@ -106,18 +44,121 @@ class Enemy {
       this.enemySprite.tint = enemyColor;
     }
 
-    this.enemySprite.width = this.enemyInformation.width;
-    this.enemySprite.height = this.enemyInformation.height;
+    let red = (enemyColor & 0xff0000) >> 16;
+    let green = (enemyColor & 0x00ff00) >> 8;
+    let blue = enemyColor & 0x0000ff;
+    let minimum = Math.min(Math.min(red, green), blue) / 255;
+    let maximum = Math.max(Math.max(red, green), blue) / 255;
+
+    this.requestedValueTextStyleToUse = new PIXI.TextStyle({
+      fontFamily:
+        '"Computer Modern Math Italic", Computer Modern Unicode Serif',
+      fill:
+        settings.video.enemyColor == "blind"
+          ? "#eeeeee"
+          : (maximum + minimum) / 2 >= 0.5
+          ? "#000000"
+          : "#ffffff",
+      fontSize: 32
+    });
+
+    this.enemySprite.width = enemyInformation.width;
+    this.enemySprite.height = enemyInformation.height;
+
+    this.requestedValueTextSprite = new PIXI.Text(
+      this.enemyInformation.requestedValue.toString().replace("-", "\u2013"),
+      this.requestedValueTextStyleToUse
+    );
+    this.requestedValueTextMetrics = PIXI.TextMetrics.measureText(
+      this.enemyInformation.requestedValue.toString(),
+      this.requestedValueTextStyleToUse
+    );
+    this.requestedValueTextSprite.x =
+      this.enemyInformation.xPosition +
+      (this.enemyInformation.width - this.requestedValueTextMetrics.width) / 2;
+    this.requestedValueTextSprite.y =
+      this.enemyInformation.yPosition +
+      (this.enemyInformation.height - this.requestedValueTextMetrics.height) /
+        2;
+    this.requestedValueTextSprite.color =
+      enemyColor == "blind"
+        ? "#eeeeee"
+        : (maximum + minimum) / 2 >= 0.5
+        ? "#000000"
+        : "#ffffff";
+
+    // create text sprite for sender name
+    let senderNameTextStyleToUse = textStyles.SIZE_16_FONT;
+    this.senderNameTextSprite = new PIXI.Text(
+      this.enemyInformation.senderName.toString(),
+      senderNameTextStyleToUse
+    );
+    this.senderNameTextMetrics = PIXI.TextMetrics.measureText(
+      this.enemyInformation.senderName.toString(),
+      senderNameTextStyleToUse
+    );
+    this.senderNameTextSprite.x =
+      this.enemyInformation.xPosition +
+      (this.enemyInformation.width - this.senderNameTextMetrics.width) / 2;
+    this.senderNameTextSprite.y =
+      this.enemyInformation.yPosition +
+      (this.enemyInformation.height - this.senderNameTextMetrics.height) / 2 +
+      35;
+    this.senderNameTextSprite.color =
+      enemyColor == "blind" ? "#eeeeee" : enemyColor;
+
+    if (!this.minified) {
+      this.updateSprite();
+    }
+  }
+
+  checkIfOverlapping(other) {
+    let thisLeft = this.xPosition;
+    let thisRight = this.xPosition + this.width;
+
+    let thisTop = this.yPosition;
+    let thisBottom = this.yPosition + this.height;
+
+    let otherLeft = other.xPosition;
+    let otherRight = other.xPosition + other.width;
+
+    let otherTop = other.yPosition;
+    let otherBottom = other.yPosition + other.height;
+
+    if (
+      thisLeft < otherRight &&
+      thisRight > otherLeft // &&
+      // thisTop < otherBottom &&
+      // thisBottom > otherTop
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  countOverlapping() {
+    let total = 0;
+    for (let enemy in game.enemyRenderStatus) {
+      if (enemy.enemyNumber === this.enemyNumber) {
+        // don't compare with itself
+        continue;
+      }
+      if (this.checkIfOverlapping(this, enemy)) {
+        total++;
+      }
+    }
+    return total;
+  }
+
+  updateSprite() {
+    // create text sprite for value
+
+    this.stackLevel = this.countOverlapping();
+
+    // debug only
+    this.requestedValueTextSprite.text = this.stackLevel;
 
     if (this.stackLevel === 0) {
-      this.requestedValueTextSprite = new PIXI.Text(
-        this.enemyInformation.requestedValue.toString().replace("-", "\u2013"),
-        requestedValueTextStyleToUse
-      );
-      this.requestedValueTextMetrics = PIXI.TextMetrics.measureText(
-        this.enemyInformation.requestedValue.toString(),
-        requestedValueTextStyleToUse
-      );
       this.requestedValueTextSprite.x =
         this.enemyInformation.xPosition +
         (this.enemyInformation.width - this.requestedValueTextMetrics.width) /
@@ -126,23 +167,7 @@ class Enemy {
         this.enemyInformation.yPosition +
         (this.enemyInformation.height - this.requestedValueTextMetrics.height) /
           2;
-      this.requestedValueTextSprite.color =
-        enemyColor == "blind"
-          ? "#eeeeee"
-          : (maximum + minimum) / 2 >= 0.5
-          ? "#000000"
-          : "#ffffff";
 
-      // create text sprite for sender name
-      let senderNameTextStyleToUse = textStyles.SIZE_16_FONT;
-      this.senderNameTextSprite = new PIXI.Text(
-        this.enemyInformation.senderName.toString(),
-        senderNameTextStyleToUse
-      );
-      this.senderNameTextMetrics = PIXI.TextMetrics.measureText(
-        this.enemyInformation.senderName.toString(),
-        senderNameTextStyleToUse
-      );
       this.senderNameTextSprite.x =
         this.enemyInformation.xPosition +
         (this.enemyInformation.width - this.senderNameTextMetrics.width) / 2;
@@ -150,8 +175,17 @@ class Enemy {
         this.enemyInformation.yPosition +
         (this.enemyInformation.height - this.senderNameTextMetrics.height) / 2 +
         35;
-      this.senderNameTextSprite.color =
-        enemyColor == "blind" ? "#eeeeee" : enemyColor;
+    } else {
+      this.requestedValueTextSprite.y =
+        this.enemyInformation.yPosition +
+        (this.enemyInformation.height - this.requestedValueTextMetrics.height) /
+          2 +
+        this.stackLevel * 40;
+      this.senderNameTextSprite.y =
+        this.enemyInformation.yPosition +
+        (this.enemyInformation.height - this.senderNameTextMetrics.height) / 2 +
+        35 +
+        this.stackLevel * 40;
     }
   }
 }
