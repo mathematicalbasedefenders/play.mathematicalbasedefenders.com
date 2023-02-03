f3775b0795cdb065b165df94e5b8b8a5d0a9c7ff
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-02-03 23:43:06 +0700

    change: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,24 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/public/js/index.ts",
+  externals: {
+    "PIXI": "pixi.js",
+    "fontfaceobserver": "FontFaceObserver",
+    "adaptive-scale": "adaptive-scale"
+  },
   target: "web",
-  mode: "production",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.ts?$/,
         use: "ts-loader",
-        exclude: [/node_modules/, /uWebSocket/, /adaptive-scale/]
+        exclude: [/node_modules/, /uWebSocket/, /adaptive-scale/, /pixi/]
       },
       {
         test: /\.ts?$/,
         use: "ts-loader",
-        exclude: [/node_modules/, /uWebSocket/, /adaptive-scale/],
+        exclude: [/node_modules/, /uWebSocket/, /adaptive-scale/, /pixi/],
         include: ["/src/public/js/*"]
       }
     ]
