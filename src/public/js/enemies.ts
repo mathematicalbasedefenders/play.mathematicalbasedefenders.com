84da0610ffb033ebb1359ed068c3a162cd9a8ac0
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-02-03 22:20:46 +0700

    change: moved enemy-related code to new  file

---
+++ src/public/js/enemies.ts
@@ -0,0 +1,66 @@
+import { Sprite, Texture } from "pixi.js";
+import { app } from ".";
+
+// file handles all enemy-related stuff
+let enemiesDrawn: Array<string> = [];
+let enemyCache: Array<Enemy> = [];
+class Enemy {
+  sprite!: Sprite;
+  text!: Text;
+  sPosition!: number;
+  xPosition!: number;
+  yPosition!: number;
+  id!: string;
+  constructor(sPosition: number, text: string, id: string) {
+    this.sprite = new Sprite(Texture.WHITE);
+    this.sprite.tint = 0xff0000;
+    this.sprite.x = Math.random() * 640;
+    this.sprite.y = 800 - 800 * sPosition;
+
+    this.id = id;
+    enemiesDrawn.push(id);
+    enemyCache.push(this);
+  }
+  render() {
+    app.stage.addChild(this.sprite);
+  }
+  reposition(sPosition: number) {
+    this.sprite.y = 800 - 800 * sPosition;
+    if (sPosition <= 0) {
+      deleteEnemy(this.id);
+    }
+  }
+}
+function getEnemyFromCache(id: string) {
+  return enemyCache.find((enemy) => enemy.id === id);
+}
+
+function renderNewEnemy(id: string) {
+  let enemy = new Enemy(1, "666", id);
+  enemyCache.push(enemy);
+  getEnemyFromCache(id)?.render();
+}
+function repositionExistingEnemy(id: string, sPosition: number) {
+  getEnemyFromCache(id)?.reposition(sPosition);
+}
+function deleteEnemy(id: string) {
+  let enemy: Enemy | undefined = getEnemyFromCache(id);
+  let sprite: Sprite | undefined = getEnemyFromCache(id)?.sprite;
+  if (sprite) {
+    app.stage.removeChild(sprite);
+  }
+  if (enemy) {
+    enemyCache.splice(enemyCache.indexOf(enemy), 1);
+  }
+}
+function rerenderEnemy(id: string, sPosition: number) {
+  if (enemiesDrawn.indexOf(id) > -1) {
+    // enemy already drawn
+    repositionExistingEnemy(id, sPosition);
+  } else {
+    // enemy wasn't drawn yet
+    renderNewEnemy(id);
+  }
+}
+
+export { Enemy, rerenderEnemy };
--- src/public/js/game.ts
+++ src/public/js/game.ts
@@ -6,6 +6,7 @@ import {
   ExtendedText,
   socket
 } from "./index";
+import * as enemies from "./enemies";
 import { POLICY, Size, getScaledRect } from "adaptive-scale/lib-esm";
 import { Sprite, Texture } from "pixi.js";
 
@@ -20,13 +21,7 @@ function renderGameData(data: { [key: string]: any }) {
   // clear screen first
 
   for (let enemy of data.enemies) {
-    if (enemiesDrawn.indexOf(enemy.id) > -1) {
-      // enemy already drawn
-      repositionExistingEnemy(enemy.id, enemy.sPosition);
-    } else {
-      // enemy wasn't drawn yet
-      renderNewEnemy(enemy.id);
-    }
+    enemies.rerenderEnemy(enemy.id, enemy.sPosition);
   }
 }
 
@@ -52,58 +47,5 @@ function repositionStageItems() {
 window.onresize = () => {
   redrawStage();
 };
-
-// TODO: Move this to a different file
-let enemiesDrawn: Array<string> = [];
-let enemyCache: Array<Enemy> = [];
-class Enemy {
-  sprite!: Sprite;
-  text!: Text;
-  sPosition!: number;
-  xPosition!: number;
-  yPosition!: number;
-  id!: string;
-  constructor(sPosition: number, text: string, id: string) {
-    this.sprite = new Sprite(Texture.WHITE);
-    this.sprite.tint = 0xff0000;
-    this.sprite.x = Math.random() * 640;
-    this.sprite.y = 800 - 800 * sPosition;
-
-    this.id = id;
-    enemiesDrawn.push(id);
-    enemyCache.push(this);
-  }
-  render() {
-    app.stage.addChild(this.sprite);
-  }
-  reposition(sPosition: number) {
-    this.sprite.y = 800 - 800 * sPosition;
-    if (sPosition <= 0) {
-      deleteEnemy(this.id);
-    }
-  }
-}
-function getEnemyFromCache(id: string) {
-  return enemyCache.find((enemy) => enemy.id === id);
-}
-
-function renderNewEnemy(id: string) {
-  let enemy = new Enemy(1, "666", id);
-  enemyCache.push(enemy);
-  getEnemyFromCache(id)?.render();
-}
-function repositionExistingEnemy(id: string, sPosition: number) {
-  getEnemyFromCache(id)?.reposition(sPosition);
-}
-function deleteEnemy(id: string) {
-  let enemy: Enemy | undefined = getEnemyFromCache(id);
-  let sprite: Sprite | undefined = getEnemyFromCache(id)?.sprite;
-  if (sprite) {
-    app.stage.removeChild(sprite);
-  }
-  if (enemy) {
-    enemyCache.splice(enemyCache.indexOf(enemy), 1);
-  }
-}
 
 export { renderGameData };
