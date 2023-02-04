c82478455118dcc93460420bca2adca775528285
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-02-04 15:45:26 +0700

    change: add more find functions

--- src/server/core/utilities.ts
+++ src/server/core/utilities.ts
@@ -1,4 +1,7 @@
+import { connection } from "mongoose";
 import * as universal from "../universal";
+import { log } from "./log";
+import { Room } from "./Room";
 
 function checkIfPropertyWithValueExists(
   dataset: unknown,
@@ -15,4 +18,47 @@ function checkIfPropertyWithValueExists(
   return false;
 }
 
-export { checkIfPropertyWithValueExists };
+function findGameDataWithConnectionID(connectionID: string, room?: Room) {
+  // if room is defined, function jumps to finding game data from that room.
+  if (!room) {
+    log.warn(
+      "Finding game data for a connection ID with an unknown room is not supported yet."
+    );
+    return null;
+  }
+  for (let gameData of room.gameData) {
+    if (gameData.owner === connectionID) {
+      return gameData;
+    }
+  }
+  return null;
+}
+
+function findRoomWithConnectionID(
+  connectionID: string,
+  countSpectatorsToo?: boolean
+) {
+  for (let room in universal.rooms) {
+    if (countSpectatorsToo) {
+      if (
+        universal.rooms[room].memberConnectionIDs.indexOf(connectionID) > -1 ||
+        universal.rooms[room].spectatorConnectionIDs.indexOf(connectionID) > -1
+      ) {
+        return universal.rooms[room];
+      }
+    } else {
+      if (
+        universal.rooms[room].memberConnectionIDs.indexOf(connectionID) > -1
+      ) {
+        return universal.rooms[room];
+      }
+    }
+  }
+  return null;
+}
+
+export {
+  checkIfPropertyWithValueExists,
+  findRoomWithConnectionID,
+  findGameDataWithConnectionID
+};
