d9dfdc30f1828206df367b1ec082c7f3cebb3da4
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-02-09 21:25:10 +0700

    fix: fixed weird sizes

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -7,7 +7,7 @@ import { changeScreen, redrawStage } from "./game";
 let startInitTime: number = Date.now();
 const OPTIMAL_SCREEN_WIDTH: number = 1920;
 const OPTIMAL_SCREEN_HEIGHT: number = 1080;
-
+const X_POSITION_OFFSET: number = 0;
 // Fonts
 const serifFont = new FontFaceObserver("Computer Modern Unicode Serif");
 const mathFont = new FontFaceObserver("Computer Modern Math Italic");
@@ -47,7 +47,8 @@ const stageItems: stageItemsContainer = {
     playFieldBorder: new ExtendedSprite(
       PIXI.Texture.from("assets/images/playfield.png")
     ),
-    originIndicator: new ExtendedSprite(PIXI.Texture.WHITE)
+    screenTopLeftIndicator: new ExtendedSprite(PIXI.Texture.WHITE),
+    screenBottomRightIndicator: new ExtendedSprite(PIXI.Texture.WHITE)
   },
   textSprites: {
     scoreLabelText: new ExtendedText("Score", {
@@ -90,26 +91,31 @@ const stageItems: stageItemsContainer = {
 };
 
 function setContainerItemProperties() {
-  // text
-  stageItems.textSprites.scoreText.position.set(730, 645);
+  stageItems.sprites.screenTopLeftIndicator.position.set(0, 0);
+  stageItems.sprites.screenTopLeftIndicator.alpha = 0;
+  stageItems.sprites.screenBottomRightIndicator.position.set(1920, 1080);
+  stageItems.sprites.screenBottomRightIndicator.alpha = 0;
   //
-  stageItems.textSprites.scoreLabelText.position.set(730, 625);
+  stageItems.sprites.playFieldBorder.position.set(640, 160);
+  // text
+  stageItems.textSprites.scoreText.position.set(1294, 725);
+  //
+  stageItems.textSprites.scoreLabelText.position.set(1294, 705);
   stageItems.textSprites.scoreLabelText.text = "Score";
   //
   stageItems.textSprites.enemiesText.text = "Enemy Kills: 0 ≈ 0.000/s";
-  stageItems.textSprites.enemiesText.position.set(730, 715);
+  stageItems.textSprites.enemiesText.position.set(1294, 795);
   //
   stageItems.textSprites.elapsedTimeText.text = "0:00.000";
-  stageItems.textSprites.elapsedTimeText.position.set(730, 735);
+  stageItems.textSprites.elapsedTimeText.position.set(1294, 815);
   //
   stageItems.textSprites.baseHealthText.text = "♥️ 100";
-  stageItems.textSprites.baseHealthText.position.set(730, 755);
+  stageItems.textSprites.baseHealthText.position.set(1294, 835);
   //
   stageItems.textSprites.comboText.text = "";
-  stageItems.textSprites.comboText.position.set(730, 775);
+  stageItems.textSprites.comboText.position.set(1294, 855);
   //
-  stageItems.textSprites.inputText.x = 400;
-  stageItems.textSprites.inputText.y = 835;
+  stageItems.textSprites.inputText.position.set(964, 915);
   stageItems.textSprites.inputText.anchor.set(0.5, 0.5);
 }
 
