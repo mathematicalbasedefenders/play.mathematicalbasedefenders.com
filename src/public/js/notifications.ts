df5c73cf1fb8e43fbc7932991fe950b2cd504bd1
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-02-23 19:24:43 +0700

    change: t.notifs now animate but it looks ugly

--- src/public/js/notifications.ts
+++ src/public/js/notifications.ts
@@ -4,7 +4,7 @@ enum ToastNotificationPosition {
 }
 
 class ToastNotification {
-  static notifications: ToastNotification[];
+  static notifications: Array<ToastNotification> = [];
   static nextID: number = 1;
 
   text: string;
@@ -26,16 +26,31 @@ class ToastNotification {
     this.lifespan = lifespan || 5000;
     this.age = 0;
     this.render();
+    ToastNotification.notifications.push(this);
+    console.log(ToastNotification.notifications);
   }
 
   render() {
     this.renderTime = new Date();
     let id = this.id;
+    // TODO: This is only for bottom right (pos 8)
+    for (let toast of ToastNotification.notifications) {
+      if (toast.position !== this.position) {
+        continue;
+      }
+      $(`#toast-notification--${toast.id}`).animate({ bottom: "+=76" }, 500);
+    }
     $("#main-content__toast-notification-container").append(
-      `<div style='display:flex;justify-content:center;align-items:center;'class='text--centered toast-notification toast-notification__position-${this.position}' id='toast-notification--${this.id}'>${this.text}</div>`
+      `<div style='display:flex;justify-content:center;align-items:center;'class='text--centered toast-notification toast-notification--position-${this.position}' id='toast-notification--${this.id}'>${this.text}</div>`
     );
     setTimeout(function () {
       $(`#toast-notification--${id}`).remove();
+      ToastNotification.notifications.splice(
+        ToastNotification.notifications.findIndex(
+          (element) => element.id === id
+        ),
+        1
+      );
     }, this.lifespan);
   }
 }
