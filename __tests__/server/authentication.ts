f84f3544735c89ebbf641792a53f76a69a5a6638
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-06-03 16:27:17 +0700

    change: temporary disabled testing

--- __tests__/server/authentication.ts
+++ __tests__/server/authentication.ts
@@ -1,3 +1,21 @@
-describe("Authentication service", () => {
-  it("should ");
-});
+// import { User } from "../../server/src/models/User";
+// import { authenticate } from "../../server/src/authentication/authenticate";
+// import * as universal from "../../server/src/universal";
+// import uWS from "uWebSockets.js";
+// const testUser = new User({
+//   username: "Ad123",
+//   usernameInAllLowercase: "ad123",
+//   // password12345678 salted 8 times hashed
+//   password: "$2b$08$0gYM0R5QUBNqiMQ3ak9cEOlkh69yhKDr4p2MZf5TxF5GqTBiodUeS"
+// });
+// const testSocket: universal.GameSocket = {};
+// describe("Authentication service", () => {
+//   beforeAll(async () => {
+//     universal.sockets.push(testSocket);
+//     testUser.save();
+//   });
+
+//   it("should allow regular users to log in", async () => {
+//     authenticate("Ad123", "password12345678", "", true);
+//   });
+// });
