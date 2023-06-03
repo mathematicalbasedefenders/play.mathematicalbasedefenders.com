e900731b6dbb44346dfd1ea73cfd0ab8fbf2b3f9
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-06-03 18:08:32 +0700

    change: changed delay on test case

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -6,8 +6,8 @@ describe("authentication", () => {
     cy.get("#settings-screen__content--online__username").type("mistertfy64water");
     cy.get("#settings-screen__content--online__password").type("password-mistertfy64water");
     cy.get("#settings-screen__content--online__submit").click();
-    // shouldn't take longer than 3000ms
-    cy.wait(3000);
+    // shouldn't take longer than 1000ms
+    cy.wait(1000);
     cy.get("#user-account-stat--username").should("have.text", "mistertfy64water");
   });
 });
