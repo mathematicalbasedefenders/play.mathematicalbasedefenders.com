d7c6de6981af26e1c9f87d61ebb07f97d8fe0172
mistertfy64 <thathobotfy64@gmail.com>
2024-04-24 23:50:55 +0700

    change: add new field in variables object

--- client/src/public/ts/index.ts
+++ client/src/public/ts/index.ts
@@ -82,6 +82,9 @@ const variables: { [key: string]: any } = {
     timeSinceLastEnemyKill: 60 * 1000 + 1,
     level: 0,
     enemySpeedCoefficient: 1
+  },
+  navigation: {
+    focusing: null
   }
 };
 
