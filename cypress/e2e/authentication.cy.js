c62f7324792033e89c261f071a4ef2628cad341c
mistertfy64 <thathobotfy64@gmail.com>
2024-06-18 16:56:04 +0700

    change: improve auth test

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -1,41 +1,44 @@
+// set command
+Cypress.Commands.add("goToLogInField", () => {
+  cy.get("#popup-notification--1__close-button").click();
+  cy.get("#main-menu-screen-button--settings").click();
+  cy.get("#settings-screen__sidebar-item--online").click();
+});
+
 describe("authentication", () => {
+  // set authentication route
+  beforeEach(() => {
+    cy.intercept("POST", "/authenticate").as("postAuthentication");
+  });
+
   it("should allow logging users in w/ correct credentials", () => {
     cy.visit("http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
+    cy.goToLogInField();
     cy.get("#settings-screen__content--online__username").type("mistertfy64water");
     // password in testing db should be changed to this
     cy.get("#settings-screen__content--online__password").type("password-mistertfy64water");
     cy.get("#settings-screen__content--online__submit").click();
-    // shouldn't take longer than 1000ms
-    cy.wait(1000);
+    cy.wait("@postAuthentication");
     cy.get("#user-account-stat--username").should("have.text", "mistertfy64water");
   });
 
   it("should not allow logging users in w/ incorrect credentials", () => {
     cy.visit("http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
+    cy.goToLogInField();
     cy.get("#settings-screen__content--online__username").type("mistertfy64water");
     cy.get("#settings-screen__content--online__password").type("password-mistertfy64water-incorrect");
     cy.get("#settings-screen__content--online__submit").click();
-    // shouldn't take longer than 1000ms
-    cy.wait(1000);
+    cy.wait("@postAuthentication");
     cy.get("#user-account-stat--username").should("not.have.text", "mistertfy64water");
   });
 
   it("should not allow logging users in w/ invalid credentials", () => {
     cy.visit("http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
+    cy.goToLogInField();
     cy.get("#settings-screen__content--online__username").type("<script>alert(1)</script>");
     cy.get("#settings-screen__content--online__password").type("password-mistertfy64water");
     cy.get("#settings-screen__content--online__submit").click();
-    // shouldn't take longer than 1000ms
-    cy.wait(1000);
+    cy.wait("@postAuthentication");
     cy.get("#user-account-stat--username").should("not.have.text", "mistertfy64water");
   });
 });
