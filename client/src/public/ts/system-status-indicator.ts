d090dcf59d25680ba2c7646d15fb2050d8ea5cc5
mistertfy64 <thathobotfy64@gmail.com>
2024-07-24 14:24:54 +0700

    feature: code for displaying system statuses

---
+++ client/src/public/ts/system-status-indicator.ts
@@ -0,0 +1,38 @@
+function updateSystemStatusTrayText(data: { [key: string]: any }) {
+  if (data.osUsageLevel == 2) {
+    $("#status-indicators--os").css("color", "#ff0000");
+    $("#status-indicators--os").text(
+      `OS: ${Math.round(data.osUsageToShow * 100)}%`
+    );
+  } else if (data.osUsageLevel == 1) {
+    $("#status-indicators--os").css("color", "#ff6200");
+    $("#status-indicators--os").text(
+      `OS: ${Math.round(data.osUsageToShow * 100)}%`
+    );
+  } else {
+    $("#status-indicators--os").css("color", "#000000");
+    // $("#status-indicators--os").text("");
+    $("#status-indicators--os").text(
+      `OS: ${Math.round(data.osUsageToShow * 100)}%`
+    );
+  }
+
+  if (data.updateTimeLevel == 2) {
+    $("#status-indicators--update-time").css("color", "#ff0000");
+    $("#status-indicators--update-time").text(
+      `UT: ${Math.round(data.updateTimeToShow)}ms`
+    );
+  } else if (data.updateTimeLevel == 1) {
+    $("#status-indicators--update-time").css("color", "#ff6200");
+    $("#status-indicators--update-time").text(
+      `UT: ${Math.round(data.updateTimeToShow)}ms`
+    );
+  } else {
+    $("#status-indicators--update-time").css("color", "#000000");
+    $("#status-indicators--update-time").text("");
+    $("#status-indicators--update-time").text(
+      `UT: ${Math.round(data.updateTimeToShow)}ms`
+    );
+  }
+}
+export { updateSystemStatusTrayText };
