5fcbb4ed786d1465fbe4ecb6fd0dd66fc83d41b7
mistertfy64 <thathobotfy64@gmail.com>
2024-07-24 14:25:42 +0700

    change: add way to measure system status

--- server/src/core/status-indicators.ts
+++ server/src/core/status-indicators.ts
@@ -1,19 +1,33 @@
 import os from "os";
 import { log } from "./log";
+import { STATUS } from "../universal";
 
 const SYSTEM_STATUS_UPDATE_INTERVAL = 5000;
 let systemStatusUpdateTime = 0;
 
+const result = {
+  os: {
+    level: 0,
+    usage: 0
+  },
+  updateTime: {
+    level: 0,
+    time: 0
+  }
+};
+
 function updateSystemStatus(deltaTime: number) {
   systemStatusUpdateTime += deltaTime;
   if (systemStatusUpdateTime < SYSTEM_STATUS_UPDATE_INTERVAL) {
-    return;
+    return result;
   }
-  checkStatus();
   systemStatusUpdateTime -= SYSTEM_STATUS_UPDATE_INTERVAL;
+  checkStatus();
+  return result;
 }
 
 function checkStatus() {
+  // get memory usages
   const memoryUsage = process.memoryUsage();
   const usedHeapMemory = memoryUsage.heapUsed;
   const totalHeapMemory = memoryUsage.heapTotal;
@@ -21,6 +35,35 @@ function checkStatus() {
   const usedOSMemory = totalOSMemory - os.freemem();
   log.debug(`GAME: ${usedHeapMemory} / ${totalHeapMemory}`);
   log.debug(`OS: ${usedOSMemory} / ${totalOSMemory}`);
+  log.debug(`UPD: ${STATUS.lastDeltaTimeToUpdate}ms`);
+  // send status updates
+  // for os memory
+  result.os.usage = usedOSMemory / totalOSMemory;
+  result.os.level = getOSMemoryLevel(result.os.usage);
+  // for update time
+  result.updateTime.time = STATUS.lastDeltaTimeToUpdate;
+  result.updateTime.level = getUpdateTimeLevel();
+  return result;
+}
+
+function getOSMemoryLevel(usage: number) {
+  if (usage >= 0.95) {
+    return 2;
+  }
+  if (usage >= 0.85) {
+    return 1;
+  }
+  return 0;
+}
+
+function getUpdateTimeLevel() {
+  if (STATUS.lastDeltaTimeToUpdate >= 100) {
+    return 2;
+  }
+  if (STATUS.lastDeltaTimeToUpdate >= 40) {
+    return 1;
+  }
+  return 0;
 }
 
 export { updateSystemStatus };
--- server/src/index.ts
+++ server/src/index.ts
@@ -319,8 +319,8 @@ function update(deltaTime: number) {
   // CHECK FOR BAD SOCKETS
   utilities.checkWebsocketMessageSpeeds(universal.sockets, deltaTime);
   // DATA IS SENT HERE. <---
-  updateSystemStatus(deltaTime);
-  synchronizeGameDataWithSockets(deltaTime);
+  const systemStatus = updateSystemStatus(deltaTime);
+  synchronizeGameDataWithSockets(deltaTime, systemStatus || {});
 
   // delete rooms with zero players
   // additionally, delete rooms which are empty JSON objects.
@@ -345,8 +345,11 @@ function update(deltaTime: number) {
   }
 }
 
-// TODO: Move these functions somewhere else.
-function synchronizeGameDataWithSockets(deltaTime: number) {
+// TODO: Move these functions somewhere else, and also stop using any already
+function synchronizeGameDataWithSockets(
+  deltaTime: number,
+  systemStatus: { [key: string]: any }
+) {
   sendDataDeltaTime += deltaTime;
   if (sendDataDeltaTime < SYNCHRONIZATION_INTERVAL) {
     return;
@@ -354,7 +357,7 @@ function synchronizeGameDataWithSockets(deltaTime: number) {
   sendDataDeltaTime -= SYNCHRONIZATION_INTERVAL;
   for (let socket of universal.sockets) {
     synchronizeGameDataWithSocket(socket);
-    universal.synchronizeMetadataWithSocket(socket, deltaTime);
+    universal.synchronizeMetadataWithSocket(socket, deltaTime, systemStatus);
     // TODO: create a separate function for resetting `accumulatedMessages.`
   }
 }
