8151ea1d7f7c7132e74d1c7ad377e777d475eb68
mistertfy64 <thathobotfy64@gmail.com>
2024-08-01 17:39:15 +0700

    fix: fix the new test: made it use config file and follow suggestions

--- cypress/e2e/keypresses.cy.js
+++ cypress/e2e/keypresses.cy.js
@@ -1,11 +1,10 @@
 describe("keypresses", () => {
   it("should allow users to exit multiplayer room intermission through abort key", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit(Cypress.env("baseUrl") || "http://localhost:3000");
     cy.get("#popup-notification--1__close-button").click();
     cy.get("#main-menu-screen-button--multiplayer").click();
     cy.get("#multiplayer-menu-screen-button--default").click();
-    cy.wait(1000);
-    cy.get("#chat-message").type(`{esc}`);
+    cy.get("#chat-message").should("be.visible").type(`{esc}`);
     cy.get("#main-menu-screen-button--singleplayer").should("be.visible");
   });
 });
