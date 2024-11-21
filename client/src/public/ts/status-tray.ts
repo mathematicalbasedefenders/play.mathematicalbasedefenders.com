80454179a2becb6f64d2c8a02ef4e75724eb7474
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-21 16:28:05 +0700

    refactor: remove unrelated comments

--- client/src/public/ts/status-tray.ts
+++ client/src/public/ts/status-tray.ts
@@ -6,10 +6,7 @@ function updateStatusTrayText(data: { [key: string]: string }) {
   $("#status-tray-text__rooms--single").text(data.roomsSingle);
   $("#status-tray-text__rooms--multi").text(data.roomsMulti);
   $("#status-tray-text__last-update").text(data.lastUpdated);
-  // ...
   $("#status-tray-text__account--name").text(data.playerName);
-  // $("#status-tray-text__account--rank").text(data.playerRank);
-  // $("#status-tray-text__account--level").text(data.playerLevel);
   $("#status-tray-text__playing-on").text(window.location.origin);
 }
 export { updateStatusTrayText };
