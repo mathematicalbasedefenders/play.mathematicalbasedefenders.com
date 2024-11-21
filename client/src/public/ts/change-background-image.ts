645af2c15c6ddc92f31c1b53d396325799aab3fa
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-21 16:15:02 +0700

    change: add URL type + validation

--- client/src/public/ts/change-background-image.ts
+++ client/src/public/ts/change-background-image.ts
@@ -1,6 +1,10 @@
 import { ToastNotification } from "./notifications";
 
-function changeBackgroundImage(url: string) {
+function changeBackgroundImage(url: string | URL) {
+  if (!url) {
+    console.warn("No URL found for background image, resetting to #000000...");
+    return;
+  }
   try {
     document.body.style.backgroundImage = `url('${url}')`;
   } catch (error) {
