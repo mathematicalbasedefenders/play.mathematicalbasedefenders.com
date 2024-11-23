6bf3eabe43d7a2cafcee24699d5892e0cb37f998
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-23 16:35:02 +0700

    change: added a rule so that there would always be at least one enemy on the field at at all times (s.p.)

--- server/src/game/actions/update.ts
+++ server/src/game/actions/update.ts
@@ -4,6 +4,7 @@ import * as _ from "lodash";
 import { SingleplayerRoom } from "../Room";
 import { checkSingleplayerRoomClocks } from "./clocks";
 import { GameData } from "../GameData";
+import { createNewEnemy } from "../Enemy";
 
 function updateSingleplayerRoomData(room: SingleplayerRoom, deltaTime: number) {
   for (const data of room.gameData) {
@@ -13,11 +14,19 @@ function updateSingleplayerRoomData(room: SingleplayerRoom, deltaTime: number) {
     // game over?
     checkGameOverCondition(data, room);
 
+    // force spawn enemy?
+    checkForceSpawnEnemyCondition(data, room);
+
     // clocks
     checkSingleplayerRoomClocks(data, room);
   }
 }
 
+/**
+ * Moves every enemy according to the speed and attributes.
+ * @param {GameData} data The data of the room.
+ * @param {number} deltaTime The time that passed since this function for this room was last called.
+ */
 function moveEnemies(data: GameData, deltaTime: number) {
   const BASE_ENEMY_SPEED = 0.1;
   const MILLISECONDS_IN_A_SECOND = 1000;
@@ -44,6 +53,24 @@ function checkGameOverCondition(data: GameData, room: SingleplayerRoom) {
   if (data.baseHealth <= 0) {
     room.startGameOverProcess(data);
   }
+}
+
+/**
+ * Checks if a playfield has 0 enemies and spawns an new enemy if there is 0.
+ * (Singleplayer Rooms only.)
+ * @param {GameData} data The data of the room.
+ * @param {SingleplayerRoom} room The room to check the enemy count.
+ * @returns `true` if enemy count is 0 and an additional enemy is spawned, `false` otherwise.
+ */
+function checkForceSpawnEnemyCondition(data: GameData, room: SingleplayerRoom) {
+  if (data.enemies.length > 0) {
+    return false;
+  }
+
+  const enemy = createNewEnemy(`F${room.updateNumber}`);
+  data.enemies.push(_.clone(enemy));
+  data.enemiesSpawned++;
+  return true;
 }
 
 export { updateSingleplayerRoomData };
