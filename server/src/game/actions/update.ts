6a6316a3392764d0401b7aa8e86a90e8f0c72c9a
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-23 16:15:55 +0700

    refactor: extracted more enemy attributes to variable

--- server/src/game/actions/update.ts
+++ server/src/game/actions/update.ts
@@ -5,16 +5,21 @@ import { SingleplayerRoom } from "../Room";
 import { checkSingleplayerRoomClocks } from "./clocks";
 
 const BASE_ENEMY_SPEED = 0.1;
+const MILLISECONDS_IN_A_SECOND = 1000;
+
+const BASE_DAMAGE = 10;
 
 function updateSingleplayerRoomData(room: SingleplayerRoom, deltaTime: number) {
   for (let data of room.gameData) {
     // Move all the enemies down.
     for (const enemy of data.enemies) {
-      enemy.move(
-        BASE_ENEMY_SPEED * data.enemySpeedCoefficient * (deltaTime / 1000)
-      );
+      const distance =
+        BASE_ENEMY_SPEED *
+        data.enemySpeedCoefficient *
+        (deltaTime / MILLISECONDS_IN_A_SECOND);
+      enemy.move(distance);
       if (enemy.sPosition <= 0) {
-        enemy.attackBase(data, 10);
+        enemy.attackBase(data, BASE_DAMAGE);
       }
     }
 
