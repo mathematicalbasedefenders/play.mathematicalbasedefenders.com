54bd19ab7ac3c0a647de1eebc30ba522260c8ffa
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-02-14 13:23:19 +0700

    change: change from var to const

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-var router = express.Router();
+const router = express.Router();
 import rateLimit from "express-rate-limit";
 import mongoDBSanitize from "express-mongo-sanitize";
 const limiter = rateLimit({
