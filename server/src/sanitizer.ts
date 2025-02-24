3ae3e3e4f30d8f35412cf02dcc6c6e8ea6709636
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-02-24 15:20:08 +0700

    fix: correct misspelled file name oops

--- server/src/sanitizer.ts
+++ server/src/sanitizer.ts
@@ -1,5 +1,5 @@
 const createDOMPurify = require("dompurify");
 const { JSDOM } = require("jsdom");
 const sanitizerWindow = new JSDOM("").window;
-const DOMPurify = createDOMPurify(sanitizerWindow);
-export { DOMPurify };
+const DOMPurifySanitizer = createDOMPurify(sanitizerWindow);
+export { DOMPurifySanitizer };
