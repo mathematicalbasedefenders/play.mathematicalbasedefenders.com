7ee975fab6159a9503f6da5fe56ffdc74aa17c79
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-12 13:50:08 +0700

    fix: update `keypresses.cy.js`

--- cypress/e2e/keypresses.cy.js
+++ cypress/e2e/keypresses.cy.js
@@ -2,10 +2,18 @@ describe("keypresses", () => {
   it("should allow users to exit multiplayer room intermission through abort key", () => {
     cy.visit(Cypress.env("baseUrl") || "http://localhost:3000");
     cy.get("#opening-screen__play-as-guest").click();
-    cy.get("#popup-notification--1__close-button").click();
+
+    cy.get("body").then(($body) => {
+      if ($body.find("#popup-notification--1__close-button").length) {
+        cy.get("#popup-notification--1__close-button").click();
+      }
+    });
+
     cy.get("#main-menu-screen-button--multiplayer").click();
     cy.get("#multiplayer-menu-screen-button--default").click();
-    cy.get("#chat-message").should("be.visible").type(`{esc}`);
+    cy.get("#chat-message").should("be.visible");
+    cy.wait(500); // here because sometimes it doesn't work lol
+    cy.get("body").type(`{esc}`);
     cy.get("#main-menu-screen-button--singleplayer").should("be.visible");
   });
 });
