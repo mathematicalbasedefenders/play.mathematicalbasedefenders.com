5a66cd27702a01c3965584808199cbae798e4e9b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-12 13:29:16 +0700

    change: update `authentication.cy.js` to reflect new opening screen

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -1,12 +1,12 @@
 describe("authentication", () => {
   it("should allow logging users in w/ correct credentials", () => {
     cy.visit(Cypress.env("baseUrl") || "http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
-    cy.get("#settings-screen__content--online__username").type("mistertfy64water");
+    // cy.get("#popup-notification--1__close-button").click();
+    // cy.get("#main-menu-screen-button--settings").click();
+    // cy.get("#settings-screen__sidebar-item--online").click();
+    cy.get("#authentication-modal__username").type("mistertfy64water");
     // password in testing db should be changed to this
-    cy.get("#settings-screen__content--online__password").type("password-mistertfy64water");
+    cy.get("#authentication-modal__password").type("password-mistertfy64water");
     cy.get("#settings-screen__content--online__submit").click();
     // shouldn't take longer than 1000ms
     cy.wait(1000);
@@ -15,11 +15,11 @@ describe("authentication", () => {
 
   it("should not allow logging users in w/ incorrect credentials", () => {
     cy.visit(Cypress.env("baseUrl") || "http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
-    cy.get("#settings-screen__content--online__username").type("mistertfy64water");
-    cy.get("#settings-screen__content--online__password").type("password-mistertfy64water-incorrect");
+    // cy.get("#popup-notification--1__close-button").click();
+    // cy.get("#main-menu-screen-button--settings").click();
+    // cy.get("#settings-screen__sidebar-item--online").click();
+    cy.get("#authentication-modal__username").type("mistertfy64water");
+    cy.get("#authentication-modal__password").type("password-mistertfy64water-incorrect");
     cy.get("#settings-screen__content--online__submit").click();
     // shouldn't take longer than 1000ms
     cy.wait(1000);
@@ -28,11 +28,11 @@ describe("authentication", () => {
 
   it("should not allow logging users in w/ invalid credentials", () => {
     cy.visit(Cypress.env("baseUrl") || "http://localhost:3000");
-    cy.get("#popup-notification--1__close-button").click();
-    cy.get("#main-menu-screen-button--settings").click();
-    cy.get("#settings-screen__sidebar-item--online").click();
-    cy.get("#settings-screen__content--online__username").type("<script>alert(1)</script>");
-    cy.get("#settings-screen__content--online__password").type("password-mistertfy64water");
+    // cy.get("#popup-notification--1__close-button").click();
+    // cy.get("#main-menu-screen-button--settings").click();
+    // cy.get("#settings-screen__sidebar-item--online").click();
+    cy.get("#authentication-modal__username").type("<script>alert(1)</script>");
+    cy.get("#authentication-modal__password").type("password-mistertfy64water");
     cy.get("#settings-screen__content--online__submit").click();
     // shouldn't take longer than 1000ms
     cy.wait(1000);
