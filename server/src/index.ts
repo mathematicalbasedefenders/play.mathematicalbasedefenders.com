69c10f825042c13cd9c0c4096e1c6c3befb1332c
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-12 13:54:42 +0700

    change: update code to reflect renamed files

--- server/src/index.ts
+++ server/src/index.ts
@@ -69,7 +69,7 @@ app.use(
 const configurationLocation = path.join(
   __dirname,
   "..",
-  "mbd-server-configuration.json"
+  "mathematical-base-defenders-server-configuration.json"
 );
 const CONFIGURATION = JSON.parse(
   fs.readFileSync(configurationLocation, "utf-8")
