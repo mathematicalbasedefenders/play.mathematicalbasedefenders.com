adb02187ef742c031d2292266a6be1ff6cc14f65
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-13 19:42:14 +0700

    change: better template literals

--- server/src/core/log.ts
+++ server/src/core/log.ts
@@ -12,19 +12,19 @@ const log: {
 } = {
   error: (...message: Array<string>) => {
     const timestamp = new Date().toISOString();
-    console.error(`${"[" + timestamp + " ERROR]"} ${message.join(" ")}`);
+    console.error(`[${timestamp} ERROR]"} ${message.join(" ")}`);
   },
   warn: (...message: Array<string>) => {
     const timestamp = new Date().toISOString();
-    console.warn(`${"[" + timestamp + " WARN]"} ${message.join(" ")}`);
+    console.warn(`[${timestamp} WARN]"} ${message.join(" ")}`);
   },
   info: (...message: Array<string>) => {
     const timestamp = new Date().toISOString();
-    console.info(`${"[" + timestamp + " INFO]"} ${message.join(" ")}`);
+    console.info(`[${timestamp} INFO]"} ${message.join(" ")}`);
   },
   debug: (...message: Array<string>) => {
     const timestamp = new Date().toISOString();
-    console.debug(`${"[" + timestamp + " DEBUG]"} ${message.join(" ")}`);
+    console.debug(`[${timestamp} DEBUG]"} ${message.join(" ")}`);
   }
 };
 
