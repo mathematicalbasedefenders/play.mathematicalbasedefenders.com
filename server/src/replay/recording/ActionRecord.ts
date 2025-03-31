8fb590189c2e1a619ba58071001d7b9a110e4379
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-31 20:44:24 +0700

    feature: basic `GameActionLog` class

--- server/src/replay/recording/ActionRecord.ts
+++ server/src/replay/recording/ActionRecord.ts
@@ -1,5 +1,64 @@
-type ActionRecord = {
-  action: string;
+import { GameSocket } from "../../universal";
+
+enum Action {
+  /** Direct by player */
+  Input = "input",
+  Submission = "submission",
+
+  /** Indirect by player */
+  EnemyKill = "enemyKill",
+  EnemySend = "enemySend",
+  StockCancel = "stockCancel",
+  StockRelease = "stockRelease",
+
+  /** Events from server */
+  EnemyReceive = "enemyReceive",
+  EnemySpawn = "enemySpawn",
+  LevelUp = "levelUp",
+
+  /** "Meta" events */
+  GameStart = "gameStart",
+  GameOver = "gameOver"
+}
+
+class GameActionLog {
+  actionRecords: Array<ActionRecord>;
+  version: number;
+  constructor() {
+    this.version = 1;
+    this.actionRecords = [];
+    this.initialize();
+  }
+
+  /**
+   * Initializes the recording phase of the log.
+   * Should be called when a game starts.
+   */
+  initialize() {
+    const startActionRecord: ActionRecord = {
+      scope: "room",
+      action: Action.GameStart,
+      timestamp: Date.now(),
+      data: {}
+    };
+    this.addAction(startActionRecord);
+  }
+
+  addAction(action: ActionRecord) {
+    this.actionRecords.push();
+  }
+}
+
+interface ActionRecord {
+  scope: "room" | "player";
+  user?: {
+    userID: string | null;
+    name: string;
+    isAuthenticated: boolean;
+  };
+  action: Action;
   timestamp: number;
   data: { [key: string]: any };
-};
+}
+
+export { GameActionLog, Action, ActionRecord };
