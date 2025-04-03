eef28d90f19481a5837a38e04e369c8ff0ad7617
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-04-03 13:24:34 +0700

    feature: `GameActionRecord` model for mongoose

---
+++ server/src/models/GameActionRecord.ts
@@ -0,0 +1,37 @@
+import mongoose, {
+  HydratedDocument,
+  ObjectId,
+  Schema,
+  SchemaTypes
+} from "mongoose";
+import _ from "lodash";
+import { log } from "../core/log";
+import { ActionRecord } from "../replay/recording/ActionRecord";
+
+interface GameActionRecordInterface {
+  _id: ObjectId;
+  data: Array<ActionRecord>;
+  replayVersion: number;
+  gameVersion: string;
+  replayOwner: ObjectId;
+}
+
+interface GameActionRecordModel
+  extends mongoose.Model<GameActionRecordInterface> {}
+
+const GameActionRecordSchema = new mongoose.Schema<
+  GameActionRecordInterface,
+  GameActionRecordModel
+>({
+  data: Array<ActionRecord>,
+  replayVersion: Number,
+  gameVersion: String,
+  replayOwner: SchemaTypes.ObjectId
+});
+
+const GameActionRecord = mongoose.model<
+  GameActionRecordInterface,
+  GameActionRecordModel
+>("GameActionRecord", GameActionRecordSchema, "gameActionRecords");
+
+export { GameActionRecord, GameActionRecordInterface };
