14258cf8de06ab64e342fa6c82d653c2b979fd20
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-10 20:25:05 +0700

    change: basic (broken) replay functionality

--- client/src/public/ts/replay.ts
+++ client/src/public/ts/replay.ts
@@ -1,4 +1,5 @@
 import { changeScreen, renderGameData } from "./game";
+import { resetClientSideVariables } from "./rendering";
 
 interface Replay {
   ok: boolean;
@@ -16,8 +17,134 @@ async function fetchReplay(replayID: string) {
 
 function playReplay(replayData: Replay) {
   const data = replayData.data;
-  changeScreen("canvas");
-  renderGameData(data.actionRecords[12]);
+  changeScreen("canvas", true, true);
+  const replayGameData: { [key: string]: any } = {
+    commands: {},
+    aborted: false
+  };
+  const dataLength = data.actionRecords.length;
+
+  // TODO: actionNumber == 0 ?
+  // FIXME: also get mode from server (this is a placeholder)
+  replayGameData.mode = "standardSingleplayer";
+
+  for (let actionNumber = 0; actionNumber < dataLength; actionNumber++) {
+    updateReplayGameData(replayGameData, data, actionNumber);
+    console.log(replayGameData);
+    renderGameData(replayGameData);
+  }
+}
+
+function updateReplayGameData(
+  replayGameData: { [key: string]: any },
+  data: { [key: string]: any },
+  actionNumber: number
+) {
+  // set time
+
+  if (actionNumber === 0) {
+    replayGameData.elapsedTime = 0;
+  } else {
+    replayGameData.elapsedTime +=
+      data.actionRecords[actionNumber].timestamp -
+      data.actionRecords[actionNumber - 1].timestamp;
+  }
+
+  const actionRecord = data.actionRecords[actionNumber];
+
+  switch (actionRecord.action) {
+    case "keypress": {
+      break;
+    }
+    case "submit": {
+      break;
+    }
+    case "enemyKill": {
+      break;
+    }
+    case "attack": {
+      break;
+    }
+    case "stockCancel": {
+      break;
+    }
+    case "stockAdd": {
+      break;
+    }
+    case "stockRelease": {
+      break;
+    }
+    case "enemyReceive": {
+      break;
+    }
+    case "enemySpawn": {
+      const enemyData = actionRecord.data;
+      replayGameData.enemies.push(enemyData);
+      break;
+    }
+    case "enemyReachedBase": {
+      break;
+    }
+    case "gameStart": {
+      resetClientSideVariables();
+      replayGameData.score = 0;
+      replayGameData.enemiesKilled = 0;
+      replayGameData.enemiesSpawned = 0;
+      replayGameData.baseHealth = 100;
+      replayGameData.owner = {};
+      replayGameData.ownerConnectionID = "";
+      replayGameData.ownerName = "";
+      replayGameData.enemies = [];
+      replayGameData.enemiesToErase = [];
+      replayGameData.currentInput = "";
+      replayGameData.elapsedTime = 0;
+      replayGameData.combo = -1;
+      replayGameData.commands = {};
+      replayGameData.aborted = false;
+      replayGameData.enemiesSentStock = 0;
+      replayGameData.attackScore = 0;
+      replayGameData.level = 1;
+      replayGameData.enemiesToNextLevel = 10;
+      replayGameData.baseHealthRegeneration = 2;
+      replayGameData.maximumBaseHealth = 100;
+      replayGameData.actionsPerformed = 0;
+      replayGameData.clocks = {
+        enemySpawn: {
+          actionTime: 0,
+          currentTime: 0
+        },
+        comboReset: {
+          actionTime: 0,
+          currentTime: 0
+        },
+        forcedEnemySpawn: {
+          actionTime: 0,
+          currentTime: 0
+        }
+      };
+      replayGameData.enemySpeedCoefficient = 1;
+      replayGameData.enemySpawnThreshold = 0.1;
+      break;
+    }
+    case "gameOver": {
+      break;
+    }
+    case "elimination": {
+      break;
+    }
+    case "declareWinner": {
+      break;
+    }
+    case "addUser": {
+      replayGameData.owner = actionRecord.data.name;
+      replayGameData.userID = actionRecord.data.userID;
+      break;
+    }
+    case "setGameData": {
+      replayGameData[actionRecord.data.key] = actionRecord.data.value;
+      break;
+    }
+  }
 }
 
 export { fetchReplay, playReplay, Replay };
