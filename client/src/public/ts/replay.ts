91857de1820fcbe8fd3220a97a92c66d8f3e1607
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-10 19:04:54 +0700

    feature: placeholder play replay function

--- client/src/public/ts/replay.ts
+++ client/src/public/ts/replay.ts
@@ -1,7 +1,17 @@
+interface Replay {
+  ok: boolean;
+  reason: string;
+  data: { [key: string]: unknown };
+}
+
 async function fetchReplay(replayID: string) {
   const url = `/api/replay${replayID}`;
   const data = await fetch(url);
   return data;
 }
 
-export { fetchReplay };
+function playReplay(replayData: Replay) {
+  const data = replayData.data;
+}
+
+export { fetchReplay, playReplay, Replay };
