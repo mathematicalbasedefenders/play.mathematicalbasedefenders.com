99b070b1324ae4cf90fb15feeaa255feef0efac6
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-10 18:45:26 +0700

    feature: fetch replay for real

--- client/src/public/ts/replay.ts
+++ client/src/public/ts/replay.ts
@@ -1,3 +1,6 @@
-function fetchReplay(replayID: string) {}
+async function fetchReplay(replayID: string) {
+  const url = `/api/replay${replayID}`;
+  const data = await fetch(url);
+}
 
 export { fetchReplay };
