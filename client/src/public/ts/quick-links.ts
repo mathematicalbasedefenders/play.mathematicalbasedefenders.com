dd5531eb39d11f7cf8cb8da0f8d24fb93214d49b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-06-02 16:46:19 +0700

    feature: first iteration of replay quick link

--- client/src/public/ts/quick-links.ts
+++ client/src/public/ts/quick-links.ts
@@ -1,22 +1,35 @@
+import { changeScreen } from "./game";
+
 function checkQuickLink() {
   const QUICK_LINKS = ["replayID"];
   const parameters = new URLSearchParams(window.location.search);
   for (let parameter of QUICK_LINKS) {
     if (parameters.get(parameter)) {
-      redirectScreen(parameter, parameters.get(parameter));
+      activateLink(parameter, parameters.get(parameter));
     }
   }
 }
 
-function redirectScreen(parameter: string, value: string | null) {
+function activateLink(parameter: string, value: string | null) {
   if (!value) {
     return;
   }
   switch (parameter) {
     case "replay": {
+      const REPLAY_REGEX = /[0-9a-f]{24}/;
+      if (!REPLAY_REGEX.test(value)) {
+        break;
+      }
+      redirectToReplay(value);
       break;
     }
   }
+}
+
+function redirectToReplay(replayID: string) {
+  changeScreen("archiveMenu");
+  $("#archive__replay-id").val(replayID);
+  $("#archive__search-button").trigger("click");
 }
 
 export { checkQuickLink };
