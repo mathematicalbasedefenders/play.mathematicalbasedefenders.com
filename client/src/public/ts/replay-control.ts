32f71d2ec5dae9ee7b65d72145ae2a493b562c0d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-06-19 19:06:06 +0700

    feature: initial implementation of seamless enemy control on jump feature

--- client/src/public/ts/replay-control.ts
+++ client/src/public/ts/replay-control.ts
@@ -1,4 +1,14 @@
 import { variables } from ".";
+import { ActionRecord, Replay } from "./replay";
+
+interface ReplayEnemyContext {
+  ignored: Array<string>;
+  setPositions: { [key: string]: number };
+}
+
+interface ReplayContext {
+  enemies: ReplayEnemyContext;
+}
 
 function controlReplay(code: string) {
   switch (code) {
@@ -36,4 +46,84 @@ function jumpToTimeInReplay(destination: number) {
   variables.replay.jumped = true;
 }
 
-export { controlReplay };
+/**
+ * Gets additional replay context
+ * TODO: Expand what the above sentence means.
+ * @param actionRecords
+ * @param actionNumbers
+ * @param time Current time of the replay
+ */
+function getReplayContext(
+  actionRecords: Array<ActionRecord>,
+  actionNumbers: Array<number>,
+  time: number
+) {
+  const context: ReplayContext = {
+    enemies: {
+      ignored: [],
+      setPositions: {}
+    }
+  };
+  context.enemies = getEnemiesReplayContext(actionRecords, actionNumbers, time);
+  return context;
+}
+
+function getEnemiesReplayContext(
+  actionRecords: Array<ActionRecord>,
+  actionNumbers: Array<number>,
+  time: number
+) {
+  const aliveEnemies: {
+    [key: string]: { spawnTimestamp: number; speed: number };
+  } = {};
+  const enemies: ReplayEnemyContext = {
+    ignored: [],
+    setPositions: {}
+  };
+  /**
+   * An enemy is ignored when it was already killed/reached base between the range of actions.
+   * This way the enemy doesn't appear and mess up anything.
+   */
+  for (const actionNumber of actionNumbers) {
+    // already killed
+    if (
+      actionRecords[actionNumber].action === "enemyKill" &&
+      Object.keys(aliveEnemies).find(actionRecords[actionNumber].data.enemyID)
+    ) {
+      enemies.ignored.push(actionRecords[actionNumber].data.enemyID);
+      delete aliveEnemies[actionRecords[actionNumber].data.enemyID];
+      continue;
+    }
+    // already reached base
+    if (
+      actionRecords[actionNumber].action === "enemyReachedBase" &&
+      Object.keys(aliveEnemies).find(actionRecords[actionNumber].data.enemyID)
+    ) {
+      enemies.ignored.push(actionRecords[actionNumber].data.enemyID);
+      delete aliveEnemies[actionRecords[actionNumber].data.enemyID];
+      continue;
+    }
+    // new spawn
+    if (actionRecords[actionNumber].action === "enemySpawn") {
+      aliveEnemies[actionRecords[actionNumber].data.id] = {
+        spawnTimestamp: actionRecords[actionNumber].timestamp,
+        speed: actionRecords[actionNumber].data.speed
+      };
+    }
+  }
+  /**
+   * An enemy is moved some position down because the replay system doesn't implement it.
+   * This will implement the function where the enemies "spawn" at sPosition < 1
+   * This just instantly moves the enemy to sPosition in zero time.
+   * TODO: This doesn't take care of when level up yet (that is, when enemy speed changes for all enemies.)
+   */
+  const startTimestamp = actionRecords[0].timestamp;
+  for (const enemy of Object.keys(aliveEnemies)) {
+    const timeElapsed =
+      time - (aliveEnemies[enemy].spawnTimestamp - startTimestamp);
+    enemies.setPositions[enemy] = 1 - timeElapsed * aliveEnemies[enemy].speed;
+  }
+  return enemies;
+}
+
+export { controlReplay, getReplayContext };
