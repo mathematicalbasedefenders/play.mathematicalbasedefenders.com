e830992675e029409a208309fcd3ee0d5d6d7085
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-15 15:29:33 +0700

    change: update version numbers

--- server/src/replay/recording/ActionRecord.ts
+++ server/src/replay/recording/ActionRecord.ts
@@ -41,7 +41,7 @@ class GameActionRecord {
   constructor() {
     this.recordingVersion = 1;
     // TODO: temp
-    this.gameVersion = "0.5.0-rc.5";
+    this.gameVersion = "0.5.0-rc.6";
     this.actionRecords = [];
     this.owner = null;
   }
