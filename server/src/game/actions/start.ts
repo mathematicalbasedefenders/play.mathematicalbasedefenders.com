cf316ded4a15587b8458b1038d430b2fe688ff92
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-29 13:27:27 +0700

    fix: remove unused items

--- server/src/game/actions/start.ts
+++ server/src/game/actions/start.ts
@@ -5,19 +5,3 @@ enum SingleplayerGameMode {
   STANDARD = 2,
   CUSTOM = 3
 }
-
-type SingleplayerGameParameters = {
-  mode: SingleplayerGameMode;
-  data: {
-    game: {
-      baseHealth: number;
-      combo: number;
-      score: number;
-      enemies: {
-        stock: Array<Enemy>;
-        onField: Array<Enemy>;
-      };
-      enemySpeedMultiplier: number;
-    };
-  };
-};
