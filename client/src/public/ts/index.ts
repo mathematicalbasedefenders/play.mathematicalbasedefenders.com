914eb91b09c17ba8e62010e04bd6f918e5f5aaba
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-09-29 00:15:37 +0700

    fix: remove `isPlaying` condition for user menu pop up to show

--- client/src/public/ts/index.ts
+++ client/src/public/ts/index.ts
@@ -770,11 +770,7 @@ function initializeEventListeners() {
     $("#chat-tray-input").val("");
   });
   $(`#main-content__user-menu-small-display`).on("click", () => {
-    if (
-      variables.isGuest ||
-      variables.playing ||
-      variables.loggedInUserID == null
-    ) {
+    if (variables.isGuest || variables.loggedInUserID == null) {
       return;
     }
     showUserLookupPopUp(variables.loggedInUserID);
