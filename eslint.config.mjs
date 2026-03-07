35c40048e87115f2263e0c9b288586580f6df15d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-07 21:31:30 +0700

    chore: update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  globalIgnores([
+    "**/*/tests/*",
+    "**/*/cypress/**/*",
+    "client/src/public/lib/*",
+    "**/*/anticheat.ts",
+    "**/*/disabled-anticheat.ts"
+  ]),
+  {
+    rules: {
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
+    }
+  }
+]);
