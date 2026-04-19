9bf3ba95aa4c190d9807e442359592157dd98e99
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-04-19 18:36:48 +0700

    Revert "test: get jest working???"
    This reverts commit ddd097510dc8594f906751588ddcdb602e9c7f32.

--- tests/jest/globalTeardown.ts
+++
@@ -1,8 +0,0 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-
-module.exports = async function () {
-  console.log("Stopping database...");
-  const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE;
-  await instance.stop();
-  console.log("Stopped database!");
-};
