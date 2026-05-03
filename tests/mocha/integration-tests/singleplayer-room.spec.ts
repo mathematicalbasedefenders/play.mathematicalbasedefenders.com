4a20f9c241f334600978d61aeb8300b3db3836d2
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-05-03 13:49:04 +0700

    test: add beforeEach and afterEach hook to `singleplayer-room.spec.ts`

--- tests/mocha/integration-tests/singleplayer-room.spec.ts
+++ tests/mocha/integration-tests/singleplayer-room.spec.ts
@@ -1,8 +1,32 @@
 import assert from "node:assert";
 import { TESTING_CONSTANTS } from "../constants";
 import { wait } from "../wait";
+import { User } from "../../../server/src/models/User";
+import mongoose from "mongoose";
+import * as universal from "../../../server/src/universal";
+const bcrypt = require("bcrypt");
 
 describe("SingleplayerRoom", () => {
+  let databaseConnection: mongoose.Mongoose;
+
+  beforeEach(async function () {
+    databaseConnection = await mongoose.connect(process.env.MONGO_URI ?? "");
+    mongoose.connection.on("connected", () => {
+      console.log(`Connected to test database!`);
+    });
+    (globalThis as any).sockets = [];
+    (globalThis as any).rooms = [];
+    universal.STATUS.databaseAvailable = true;
+
+    // add test user
+    const user = new User();
+    user.username = TESTING_CONSTANTS.TESTING_USER_USERNAME;
+    user.hashedPassword = await bcrypt.hash(
+      TESTING_CONSTANTS.TESTING_USER_PASSWORD,
+      4
+    );
+    await user.save();
+  });
   it("should create a room once the constructor is called", async () => {
     const url = `ws://localhost:${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`;
     const socket = new WebSocket(url);
@@ -29,5 +53,10 @@ describe("SingleplayerRoom", () => {
 
     assert.ok((globalThis as any).rooms);
     assert.equal((globalThis as any).rooms.length, 1);
+  });
+
+  afterEach(async function () {
+    await databaseConnection.connection.db.dropDatabase();
+    await databaseConnection.connection.close();
   });
 });
