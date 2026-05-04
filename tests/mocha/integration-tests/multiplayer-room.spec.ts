22591072a149d1e4fd22c4c55db9ae9e511edeb5
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-05-04 18:37:16 +0700

    test: add start multiplayer room command testcase
    i.e., `/start` to start a custom multiplayer room

--- tests/mocha/integration-tests/multiplayer-room.spec.ts
+++ tests/mocha/integration-tests/multiplayer-room.spec.ts
@@ -187,6 +187,86 @@ describe("MultiplayerRoom", () => {
     socket2.close();
   });
 
+  it("should be able to start a multiplayer game if there are 2 or more players and the host chooses to start", async () => {
+    const url = `ws://localhost:${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`;
+    const socket1 = new WebSocket(url);
+
+    await new Promise((resolve) => socket1.addEventListener("open", resolve));
+
+    const exitOpeningScreenMessage = {
+      message: { message: "exitOpeningScreen" }
+    };
+
+    const createRoomMessage = {
+      message: {
+        message: "createMultiplayerRoom"
+      }
+    };
+
+    socket1.send(JSON.stringify(exitOpeningScreenMessage));
+    socket1.send(JSON.stringify(createRoomMessage));
+
+    await waitForWebSocketMessage(
+      socket1,
+      (data: { [key: string]: unknown }) => {
+        return (
+          data.message === "changeScreen" &&
+          data.newScreen === "customMultiplayerIntermission"
+        );
+      }
+    );
+
+    assert.ok((globalThis as any).rooms);
+    assert.equal((globalThis as any).rooms.length, 1);
+
+    const roomID = (globalThis as any).rooms[0].id;
+
+    const socket2 = new WebSocket(url);
+    await new Promise((resolve) => socket2.addEventListener("open", resolve));
+    const joinRoomMessage = {
+      message: {
+        message: "joinMultiplayerRoom",
+        room: roomID
+      }
+    };
+
+    socket2.send(JSON.stringify(exitOpeningScreenMessage));
+    socket2.send(JSON.stringify(joinRoomMessage));
+
+    await waitForWebSocketMessage(
+      socket2,
+      (data: { [key: string]: unknown }) => {
+        return (
+          data.message === "changeScreen" &&
+          data.newScreen === "customMultiplayerIntermission"
+        );
+      }
+    );
+
+    assert.equal((globalThis as any).rooms[0].memberConnectionIDs.length, 2);
+
+    const startGameMessage = {
+      message: {
+        message: "sendChatMessage",
+        scope: "room",
+        chatMessage: "/start"
+      }
+    };
+    socket1.send(JSON.stringify(startGameMessage));
+
+    await waitForWebSocketMessage(
+      socket1,
+      (data: { [key: string]: unknown }) => {
+        return data.message === "changeScreen" && data.newScreen === "canvas";
+      }
+    );
+
+    assert.equal((globalThis as any).rooms[0].playing, true);
+
+    socket1.close();
+    socket2.close();
+  });
+
   afterEach(async function () {
     await databaseConnection.connection.db.dropDatabase();
     await databaseConnection.connection.close();
