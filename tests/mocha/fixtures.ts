a9074b6e4c328d20f701839124f6a5ab810a45b4
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-05-05 16:00:31 +0700

    test: await starting of testing instances

--- tests/mocha/fixtures.ts
+++ tests/mocha/fixtures.ts
@@ -9,28 +9,38 @@ let webSocketServer: TemplatedApp;
 let database: MongoMemoryServer;
 
 export const mochaGlobalSetup = async () => {
-  webServer = createWebServer().listen(
-    TESTING_CONSTANTS.TESTING_WEB_SERVER_PORT
-  );
+  await new Promise<void>((resolve) => {
+    webServer = createWebServer().listen(
+      TESTING_CONSTANTS.TESTING_WEB_SERVER_PORT,
+      resolve
+    );
+  });
   console.log(
     `Test server listening at port ${TESTING_CONSTANTS.TESTING_WEB_SERVER_PORT}`
   );
 
-  webSocketServer = createWebSocketServer().listen(5001, (token: any) => {
-    if (token) {
-      console.log(
-        `Test WebSockets Server listening at port ${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`
-      );
-    } else {
-      console.log(
-        `Failed to test WebSockets at port ${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`
-      );
-    }
+  await new Promise<void>((resolve, reject) => {
+    webSocketServer = createWebSocketServer().listen(5001, (token: any) => {
+      if (token) {
+        console.log(
+          `Test WebSockets Server listening at port ${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`
+        );
+        resolve();
+      } else {
+        console.log(
+          `Failed to test WebSockets at port ${TESTING_CONSTANTS.TESTING_WEBSOCKET_SERVER_PORT}`
+        );
+        reject();
+      }
+    });
   });
 
   database = await MongoMemoryServer.create({
     binary: { version: "6.0.14" }
   });
+
+  console.log(`Created test database instance.`);
+
   const uri = database.getUri();
   process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf("/"));
 };
